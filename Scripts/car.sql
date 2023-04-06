b0c8a7c8a2d55ae75b761bfdb014990039398836
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-04-06 18:18:20 -0300

    Script car.sql

---
+++ Scripts/car.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `cars` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
